4c53cfd411d253c114c7a3ca60bb6356e16ef94c
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-25 21:21:50 +0200

    Cleaned code

--- Exercises/Week 34/exercise00-3.py
+++ Exercises/Week 34/exercise00-3.py
@@ -4,12 +4,10 @@ def accuracy(tp, fp, fn, tn):
     Arg 3 takes the number of false negatives.
     Arg 4 takes the number of true negatives.
     The method returns the accuracy of the model."""
-    accuracy = (tp + tn)/(tp + tn + fp + fn)
-    return accuracy
+    return (tp + tn)/(tp + tn + fp + fn)
 
 #Predicting accuracy for a machine learning model that has been trained to predict fire. 
 #I would argue that this model is not good, because it results in significantly more false negatives than false positives.
 #In this case you would like to have a model that rather gives too many false positives, than false negatives (i.e. a model that is "overly-cautious").
 
-accuracy = accuracy(2, 2, 11, 985)
-print(accuracy)
+print(accuracy(2, 2, 11, 985))
