284149c5f304a6881d6a40a43bc5718485120c89
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-25 21:19:30 +0200

    Cleaned code

--- Exercises/Week 34/exercise00-1.py
+++ Exercises/Week 34/exercise00-1.py
@@ -8,13 +8,10 @@ def count_hypothenuse(cathetus1, cathetus2):
 def count_cathetus(cathetus1, hypothenuse):
     """Arg 1 takes the length of cathetus 1. Arg 2 takes the length of cathetus 1.
     The method returns the length of cathetus 2."""
-    cathetus2 = sqrt(hypothenuse**2 - cathetus1**2)
-    return (cathetus2)
+    return sqrt(hypothenuse**2 - cathetus1**2)
 
 #Count the length of the hypothenuse
-length_hypothenuse = count_hypothenuse(3, 4)
-print(length_hypothenuse)
+print(count_hypothenuse(3, 4))
 
 #Count the length of the cathetus
-length_cathetus = count_cathetus(5, 7)
-print(length_cathetus)
+print(count_cathetus(5, 7))
