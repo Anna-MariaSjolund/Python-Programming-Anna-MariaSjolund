3be0182e0147e64de374a06a8f6522a17b49d006
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-25 21:20:33 +0200

    Cleaned  code

--- Exercises/Week 34/exercise00-2.py
+++ Exercises/Week 34/exercise00-2.py
@@ -1,9 +1,7 @@
 def compute_accuracy(correct, total):
     """Arg 1 takes the number of correct predictions and arg 2 takes the total number of predictions.
     The model returns the accuracy of the model as a number between 0 (0%) and 1 (100%)."""
-    accuracy = correct/total
-    return(accuracy)
+    return correct/total
 
 #Count the accuracy of a model for weather prediction
-accuracy = compute_accuracy(300, 365)
-print(accuracy)
+print(compute_accuracy(300, 365))
