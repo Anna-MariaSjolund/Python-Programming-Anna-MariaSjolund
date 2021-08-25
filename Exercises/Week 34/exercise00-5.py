54b3fcbe8fd37dbf54dae35afe5009ef67d1894c
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-25 21:24:45 +0200

    Changed code

--- Exercises/Week 34/exercise00-5.py
+++ Exercises/Week 34/exercise00-5.py
@@ -3,9 +3,7 @@ from math import sqrt
 def eucl_dist (x1, x2, y1, y2):
     '''Takes the coordinates (x, y) of the two points as input.
     Method returns the euclidean distance.'''
-    distance = sqrt((x2 - x1)**2 + (y2-y1)**2)
-    return distance
+    return sqrt((x2 - x1)**2 + (y2-y1)**2)
 
 #Count the euclidean distance of a point
-eucL_distance = eucl_dist(-2, 3, 4, 5)
-print(eucL_distance)
+print(eucl_dist(-2, 3, 4, 5))
