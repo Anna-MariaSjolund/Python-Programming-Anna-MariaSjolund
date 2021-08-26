f01223f1590593fa2c9bdc117291581100855387
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-26 23:34:33 +0200

    Changed print statement to f-string

--- Exercises/Week 34/exercise00-2.py
+++ Exercises/Week 34/exercise00-2.py
@@ -3,5 +3,5 @@ def compute_accuracy(correct, total):
     The model returns the accuracy as a number between 0 (0%) and 1 (100%)."""
     return correct/total
 
-#Count the accuracy of a model for weather prediction
+#Count the accuracy of a model for weather prediction 
 print(f"The accuracy of the weather prediction model is: {compute_accuracy(300, 365):.2f}.")
