9142cc42c30f80baecbbb77ebf46bb95a1685aa7
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-26 21:51:13 +0200

    Changed description of methods

--- Exercises/Week 34/exercise00-1.py
+++ Exercises/Week 34/exercise00-1.py
@@ -6,12 +6,12 @@ def count_hypothenuse(cathetus1, cathetus2):
     return sqrt(cathetus1**2 + cathetus2**2)
 
 def count_cathetus(cathetus1, hypothenuse):
-    """Arg 1 takes the length of cathetus 1. Arg 2 takes the length of cathetus 1.
+    """Arg 1 takes the length of cathetus 1. Arg 2 takes the length of the hypothenuse.
     The method returns the length of cathetus 2."""
     return sqrt(hypothenuse**2 - cathetus1**2)
 
 #Count the length of the hypothenuse
-print(count_hypothenuse(3, 4))
+print(f"The length of the hypothenuse is {count_hypothenuse(3, 4)}.")
 
 #Count the length of the cathetus
-print(count_cathetus(5, 7))
+print(f"The length of the second cathetus is {count_cathetus(5, 7):.1f}.")
