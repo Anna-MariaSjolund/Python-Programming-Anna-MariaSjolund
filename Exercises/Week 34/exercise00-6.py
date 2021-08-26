ed51a79c2ce6832e9eacf88734de777931381e16
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-26 22:22:23 +0200

    Changed print statement to f-string

--- Exercises/Week 34/exercise00-6.py
+++ Exercises/Week 34/exercise00-6.py
@@ -1,9 +1,9 @@
 from math import sqrt
 
 def eucl_dist (x1, x2, y1, y2, z1, z2):
-    '''Takes the coordinates (x, y) of the two points as input.
-    Method returns the euclidean distance.'''
+    """Takes the coordinates (x, y, z) of the two 3D-points as input.
+    Method returns the euclidean distance."""
     return sqrt((x2 - x1)**2 + (y2-y1)**2 + (z2-z1)**2)
 
-#Count the euclidean distance of a point
-print(eucl_dist(2, 3, 1, 1, 4, 0))
+#Count the euclidean distance of the two 3D-points (2, 1, 4) and (3, 1, 0)
+print(f"The euclidean distance in 3D is: {eucl_dist(2, 3, 1, 1, 4, 0):.2f} length units.")
