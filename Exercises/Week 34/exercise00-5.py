37c84c9746b1fae09616001c8a63b5e04f0018df
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-08-26 22:15:20 +0200

    Changed print statement to f-string

--- Exercises/Week 34/exercise00-5.py
+++ Exercises/Week 34/exercise00-5.py
@@ -1,8 +1,8 @@
 from math import sqrt
 
 def eucl_dist (x1, x2, y1, y2):
-    '''Takes the coordinates (x, y) of the two points as input.
-    Method returns the euclidean distance.'''
+    """Takes the coordinates (x, y) of the two points as input.
+    Method returns the euclidean distance."""
     return sqrt((x2 - x1)**2 + (y2-y1)**2)
 
 #Count the euclidean distance of a point
