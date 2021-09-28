27817b62a78234581bb3695c4effbd4a903ef97f
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-09-28 18:09:56 +0200

    No changes

--- Labs/Lab3/geometry.py
+++ Labs/Lab3/geometry.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Geometry:
     def __init__(self, x_coordinate:float=0, y_coordinate:float=0) -> None:
         self.x_coordinate = x_coordinate
@@ -27,11 +29,26 @@ class Geometry:
             return value
     
     @staticmethod
-    def validation_above_zero(value):
-        if value <= 0:
+    def validation_numerical_above_zero(value):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Please enter an int or a float, not a {type(value)}.")
+        elif value <= 0:
             raise ValueError("The value has to be above 0.")
         else:
             return value
 
-    #is inside pythagoras sats  
-    
+    def translate(self, x_value, y_value):
+        self._x_coordinate = Geometry.validation_numerical(x_value)
+        self._y_coordinate = Geometry.validation_numerical(y_value)
+        
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def circumference(self):
+        pass
+
+    @abstractmethod
+    def is_inside(self):
+        pass
