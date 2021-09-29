305568911552fa3f143219feba437893b1c31416
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-09-29 15:29:54 +0200

    Added doc strings

--- Labs/Lab3/circle.py
+++ Labs/Lab3/circle.py
@@ -2,25 +2,83 @@ from geometry import Geometry
 import math
 
 class Circle(Geometry):
+    """
+    A class used for representing circles in a coordinate system.
+    
+    Attributes
+    ----------
+    radius : float
+        The radius of a circle.
+    x_coordinate : float
+        The x-coordinate at the centre of a circle (default 0).
+    y_coordinate : float
+        The y-coordinate at the centre of a circle (default 0).
+    
+    Methods
+    -------
+    area() -> float
+        Calculates the area of a circle.
+    circumference() -> float
+        Calculates the circumference of a circle.
+    is_inside() -> bool
+        Checks if a point (x, y) is inside a circle.
+    __eq__() -> bool
+        Checks if two circles are congruent.
+    __repr__() -> str
+        Returns information about the size and position of a circle.
+    """
+
     def __init__(self, radius:float, x_coordinate:float=0, y_coordinate:float=0) -> None:
+        """
+        Parameters
+        ----------
+        radius : float
+            The radius of a circle.
+        x_coordinate : float
+            The x-coordinate at the centre of a circle (default 0).
+        y_coordinate : float
+            The y-coordinate at the centre of a circle (default 0).
+        """
+
         super().__init__(x_coordinate, y_coordinate)
         self.radius = radius
 
-    @property
-    def radius(self) -> float:
-        return self._radius
-   
-    @radius.setter
-    def radius(self, radius) -> None:
-            self._radius = Geometry.validation_numerical_above_zero(radius)
-    
-    def area(self):
+     
+    #METHODS
+
+    def area(self) -> float:
+        """Calculates the area of a circle."""
+
         return (math.pi*(self.radius**2))
     
-    def circumference(self):
+    def circumference(self) -> float:
+        """Calculates the circumference of a circle."""
+
         return 2*math.pi*self.radius
     
-    def is_inside(self, x_value, y_value):
+    def is_inside(self, x_value:float, y_value:float) -> bool:
+        """
+        Checks if a point (x, y) is inside a circle.
+
+        Parameters
+        ----------
+        x_value : float
+            The x-value of the point.
+        y_value : float
+            The y-value of the point.
+        
+        Raises
+        ------
+        TypeError
+            If value is not int or float.
+        
+        Returns
+        -------
+        True
+            If point is in the circle.
+        False
+            If point is not in the circle.
+        """
         _ = Geometry.validation_numerical(x_value)
         _ = Geometry.validation_numerical(y_value)
         eucl_distance = math.sqrt((self.x_coordinate - x_value)**2 + (self.y_coordinate-y_value)**2)
@@ -29,7 +87,25 @@ class Circle(Geometry):
         else: 
             return False
     
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
+        """
+        Checks if two objects are congruent.
+
+        Parameters
+        ----------
+        self
+            A Circle object
+        other
+            An object for comparison.
+        
+        Returns
+        -------
+        True
+            If the radius of two Circle objects are of the same size.
+        False
+            If the other object is not a Circle.
+            If the radius of two Circle objects are not of the same size.
+        """
         if type(self) != type(other):
             return False
 
@@ -37,3 +113,40 @@ class Circle(Geometry):
             return True
         else:
             return False
+    
+    def __repr__(self):
+        """Returns information about the size and position of a circle."""
+
+        return f"The radius of the circle is: {self.radius}. The geometric center is: ({self.x_coordinate}, {self.y_coordinate})."
+
+
+    #GETTERS AND SETTERS
+    
+    @property
+    def radius(self) -> float:
+        """A getter method, returning the private radius."""
+        return self._radius
+   
+    @radius.setter
+    def radius(self, radius:float) -> None:
+        """
+        Validates that the radius is numerical and above zero, and assigns it to a private variable.
+        
+        Parameters
+        ----------
+        radius : float
+            The radius of a circle.
+        
+        Raises
+        ------
+        TypeError
+            If radius is not int or float.
+        ValueError
+            If radius is 0 or below.
+
+        Returns
+        -------
+        None
+        """
+        
+        self._radius = Geometry.validation_numerical_above_zero(radius)
