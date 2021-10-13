4768df8317064f5f655e57189d6786201ce7583c
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-10-13 22:16:48 +0200

    Changes

--- Code-Along/modules/folder1/module1.py
+++ Code-Along/modules/folder1/module1.py
@@ -2,4 +2,4 @@ def say_hello1():
     print(f"{__name__} says hello")
 
 if __name__ == "__main__":
-    say_hello1() #If not defined, __name__ gets the value __name__
+    say_hello1() #If not defined, __name__ gets the value __name__ 
--- Code-Along/modules/folder2/module2.py
+++ Code-Along/modules/folder2/module2.py
@@ -6,4 +6,4 @@ import module1
 def say_hello2():
     print(f"{__name__} says hello.")
 
-module1.say_hello1()
+module1.say_hello1() 
